64a4a23fc3a65b5fe86e226d0f636f8fa65aada4
grossmann <grossmann@movisens.com>
2017-11-13 12:37:37 +0100

    added ble characteristic sendBufferedAttribute

---
+++ src-gen/main/java/com/movisens/movisensgattlib/attributes/SendBufferedData.java
@@ -0,0 +1,44 @@
+package com.movisens.movisensgattlib.attributes;
+
+import com.movisens.movisensgattlib.MovisensCharacteristics;
+import com.movisens.smartgattlib.helper.AbstractWriteAttribute;
+import com.movisens.smartgattlib.helper.Characteristic;
+import com.movisens.smartgattlib.helper.GattByteBuffer;
+
+public class SendBufferedData extends AbstractWriteAttribute
+{
+
+	public static final Characteristic CHARACTERISTIC = MovisensCharacteristics.SEND_BUFFERED_DATA;
+	
+	private Boolean sendBufferedData;
+	
+	public Boolean getSendBufferedData()
+	{
+		return sendBufferedData;
+	}
+	
+	public String getSendBufferedDataUnit()
+	{
+		return "";
+	}
+	
+	public SendBufferedData(Boolean sendBufferedData)
+	{
+		this.sendBufferedData = sendBufferedData;
+		GattByteBuffer bb = GattByteBuffer.allocate(1);
+		bb.putBoolean(sendBufferedData);
+		this.data = bb.array();
+	}
+
+	@Override
+	public Characteristic getCharacteristic()
+	{
+		return CHARACTERISTIC;
+	}
+
+	@Override
+	public String toString()
+	{
+		return "Send Buffered Data: " + "sendBufferedData = " + getSendBufferedData();
+	}
+}
