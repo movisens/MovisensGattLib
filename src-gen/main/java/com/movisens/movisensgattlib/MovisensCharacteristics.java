143d73481f91aff77d4017fa6d14c39eae4e66c5
Ulrich Großmann <grossmann@movisens.com>
2019-09-17 11:53:42 +0200

    added time zone offset

--- src-gen/main/java/com/movisens/movisensgattlib/MovisensCharacteristics.java
+++ src-gen/main/java/com/movisens/movisensgattlib/MovisensCharacteristics.java
@@ -77,6 +77,7 @@ public class MovisensCharacteristics
 	public static final Characteristic<StepsWaiting> STEPS_WAITING = new Characteristic<StepsWaiting>("9b72b459-d1e5-48fe-9c91-2fb168261b21", "Steps Waiting", StepsWaiting.class);
 	public static final Characteristic<TapMarker> TAP_MARKER = new Characteristic<TapMarker>("207b171c-d7a5-48ef-8e60-6ccb5f0993f4", "Tap Marker", TapMarker.class);
 	public static final Characteristic<TimeZone> TIME_ZONE = new Characteristic<TimeZone>("8c3adbfa-9218-419e-b809-6de9918ba8d5", "Time Zone", TimeZone.class);
+	public static final Characteristic<TimeZoneOffset> TIME_ZONE_OFFSET = new Characteristic<TimeZoneOffset>("b9b5bd3a-475c-43a6-b25e-bc706eb016ca", "Time Zone Offset", TimeZoneOffset.class);
 
 	private static UuidObjectMap<Characteristic<? extends AbstractAttribute>> characteristics = new UuidObjectMap<Characteristic<? extends AbstractAttribute>>();
 
@@ -147,6 +148,7 @@ public class MovisensCharacteristics
 		characteristics.put(STEPS_WAITING);
 		characteristics.put(TAP_MARKER);
 		characteristics.put(TIME_ZONE);
+		characteristics.put(TIME_ZONE_OFFSET);
 	}
 	
 	/**
---
+++ src-gen/main/java/com/movisens/movisensgattlib/attributes/TimeZoneOffset.java
@@ -0,0 +1,51 @@
+package com.movisens.movisensgattlib.attributes;
+
+import com.movisens.movisensgattlib.MovisensCharacteristics;
+import com.movisens.smartgattlib.helper.AbstractReadWriteAttribute;
+import com.movisens.smartgattlib.helper.Characteristic;
+import com.movisens.smartgattlib.helper.GattByteBuffer;
+
+public class TimeZoneOffset extends AbstractReadWriteAttribute
+{
+
+	public static final Characteristic<TimeZoneOffset> CHARACTERISTIC = MovisensCharacteristics.TIME_ZONE_OFFSET;
+	
+	private Short zoneOffset;
+	
+	public Short getZoneOffset()
+	{
+		return zoneOffset;
+	}
+	
+	public String getZoneOffsetUnit()
+	{
+		return "";
+	}
+	
+	public TimeZoneOffset(Short zoneOffset)
+	{
+		this.zoneOffset = zoneOffset;
+		GattByteBuffer bb = GattByteBuffer.allocate(2);
+		bb.putInt16(zoneOffset);
+		this.data = bb.array();
+	}
+
+	public TimeZoneOffset(byte[] data)
+	{
+		this.data = data;
+		GattByteBuffer bb = GattByteBuffer.wrap(data);
+		zoneOffset = bb.getInt16();
+	}
+
+	@Override
+	public Characteristic<TimeZoneOffset> getCharacteristic()
+	{
+		return CHARACTERISTIC;
+	}
+
+	@Override
+	public String toString()
+	{
+		return getZoneOffset().toString();
+	}
+}
