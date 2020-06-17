73f9bf97a61f71e7d224c38a46c7cc6365dbd612
Ulrich Großmann <grossmann@movisens.com>
2020-06-17 18:25:06 +0200

    added more items to EnumCommandResult

--- src-gen/main/java/com/movisens/movisensgattlib/attributes/EnumCommandResult.java
+++ src-gen/main/java/com/movisens/movisensgattlib/attributes/EnumCommandResult.java
@@ -6,8 +6,10 @@ public enum EnumCommandResult
 	NOT_STARTED_BATTERY_LOW       ((short)1, "not_started_battery_low"),
 	NOT_STARTED_DATA_AVAILABLE    ((short)2, "not_started_data_available"),
 	NOT_STARTED_PROBAND_INFO_MISSING((short)3, "not_started_proband_info_missing"),
-	NOT_DELETED_MEASUREMENT_ACTIVE((short)4, "not_deleted_measurement_active"),
-	INVALID                       ((short)5, "invalid");
+	NOT_DELETED_MEASUREMENT_ON    ((short)4, "not_deleted_measurement_on"),
+	NOT_STARTED_MEASUREMENT_ON    ((short)5, "not_started_measurement_on"),
+	NOT_STOPPED_MEASUREMENT_OFF   ((short)6, "not_stopped_measurement_off"),
+	INVALID                       ((short)7, "invalid");
 
 	private final short value;
 	private final String name;
@@ -41,7 +43,11 @@ public enum EnumCommandResult
 			case 3:
 				return EnumCommandResult.NOT_STARTED_PROBAND_INFO_MISSING;
 			case 4:
-				return EnumCommandResult.NOT_DELETED_MEASUREMENT_ACTIVE;
+				return EnumCommandResult.NOT_DELETED_MEASUREMENT_ON;
+			case 5:
+				return EnumCommandResult.NOT_STARTED_MEASUREMENT_ON;
+			case 6:
+				return EnumCommandResult.NOT_STOPPED_MEASUREMENT_OFF;
 			default:
 				return EnumCommandResult.INVALID;
 		}
