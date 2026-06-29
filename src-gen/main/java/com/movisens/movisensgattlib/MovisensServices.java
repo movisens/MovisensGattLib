f00346f289ff829329b8e2235f1956dfb9e25194
Ulrich Großmann <ulrich.grossmann@movisens.com>
2026-06-29 15:28:25 +0200

    renamed eda and skin service

--- src-gen/main/java/com/movisens/movisensgattlib/MovisensServices.java
+++ src-gen/main/java/com/movisens/movisensgattlib/MovisensServices.java
@@ -9,7 +9,7 @@ public class MovisensServices
 {
 
 	public static final Service AMBIENT = new Service("d0f0f790-66c9-4e1f-bf48-1628a7ad89f9", "Ambient");
-	public static final Service EDA = new Service("eb08670d-f244-4511-b43e-04bba968b693", "Eda");
+	public static final Service EDA_SERVICE = new Service("eb08670d-f244-4511-b43e-04bba968b693", "Eda Service");
 	public static final Service HRV = new Service("0bd51666-e7cb-469b-8e4d-2742f1ba77cd", "Hrv");
 	public static final Service MARKER = new Service("32062bba-7843-4ad6-94ea-95c66909edcf", "Marker");
 	public static final Service MOVISENS_BATTERY = new Service("27b66685-62e5-4e76-8c02-d625600ed2c6", "Movisens Battery");
@@ -17,14 +17,14 @@ public class MovisensServices
 	public static final Service PHYSICAL_ACTIVITY = new Service("0302c2b2-ce64-4542-b819-666d20d415bd", "Physical Activity");
 	public static final Service RESPIRATION = new Service("da87d1a7-749c-4711-bd54-c625043ecd83", "Respiration");
 	public static final Service SENSOR_CONTROL = new Service("f89edeb6-e4e8-928b-4cfa-ebc07fce1768", "Sensor Control");
-	public static final Service SKIN_TEMPERATURE = new Service("247af432-444c-4211-8b9d-2c8512cfdf4a", "Skin Temperature");
+	public static final Service SKIN_SERVICE = new Service("247af432-444c-4211-8b9d-2c8512cfdf4a", "Skin Service");
 
 	private static UuidObjectMap<Service> services = new UuidObjectMap<Service>();
 
 	static
 	{
 		services.put(AMBIENT);
-		services.put(EDA);
+		services.put(EDA_SERVICE);
 		services.put(HRV);
 		services.put(MARKER);
 		services.put(MOVISENS_BATTERY);
@@ -32,7 +32,7 @@ public class MovisensServices
 		services.put(PHYSICAL_ACTIVITY);
 		services.put(RESPIRATION);
 		services.put(SENSOR_CONTROL);
-		services.put(SKIN_TEMPERATURE);
+		services.put(SKIN_SERVICE);
 	}
 
 	/**
